fe4585ec2a6d2b909476ab9d0ad9a20ed4e852cd
signsofdidier <signsofdidier@gmail.com>
2025-02-13 15:35:40 +0100

    first upload

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('frontend.home');
+});
+
+//Route::middleware(['auth'])->group(function(){
+// Route::resource('/backend/users', UserController::class);
+//});
+Route::group(['prefix'=>'backend','middleware'=>'auth'],function(){
+    Route::resource('/users', UserController::class);
+});
+
+/*Route::resource('/backend/users', UserController::class);*/
+
+Route::get('/backend', function () {
+    return view('backend.dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+require __DIR__.'/auth.php';
